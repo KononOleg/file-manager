faf6ee87c24f63fce3d164255126a4a6036f2537
KononOleg <konon.oleg97@gmail.com>
2022-12-19 09:44:26 +0300

    fix: os info

--- src/handlers/os/index.js
+++ src/handlers/os/index.js
@@ -1,12 +1,10 @@
 import os from "os";
 
 const osInfo = async (type) => {
-  const { write } = process.stdout;
-
   try {
     switch (type) {
       case "EOL":
-        write("\n" + "EOL: " + JSON.stringify(os.EOL));
+        process.stdout.write("\n" + "EOL: " + JSON.stringify(os.EOL));
         break;
       case "cpus":
         const result = [];
@@ -19,19 +17,19 @@ const osInfo = async (type) => {
         console.table(result);
         break;
       case "homedir":
-        write(`\nHomedir: ${os.homedir}\n`);
+        process.stdout.write(`\nHomedir: ${os.homedir}\n`);
         break;
       case "username":
-        write(`\nUsername: ${os.userInfo().username}\n`);
+        process.stdout.write(`\nUsername: ${os.userInfo().username}\n`);
         break;
       case "architecture":
-        write(`\nArchitecture: ${os.arch}\n`);
+        process.stdout.write(`\nArchitecture: ${os.arch}\n`);
         break;
       default:
-        write("\nOperation failed\n");
+        process.stdout.write("\nOperation failed\n");
     }
   } catch {
-    write("\nOperation failed\n");
+    process.stdout.write("\nOperation failed\n");
   }
 };
 export default osInfo;
